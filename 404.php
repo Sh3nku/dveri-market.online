38e72784fbe6736d2438da0f3d95f8418b4c6d45
Sh3nku <sh3nku@gmail.com>
2023-07-28 13:53:20 +0200

    404 страница

--- 404.php
+++ 404.php
@@ -1,8 +1,30 @@
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/admin/header.php';
 
-$MAIN -> SetTitle( '404 Страница не найдена' );
-$BUFFER -> SetBuffer( 'H1', '404' );
+$MAIN -> SetTitle( '404 Страница не найдена' )?>
 
-echo '<div style="font-size: 100px; line-height: 100px">404</div>';
+<section class="page-404">
+    <div class="container">
+        <div class="page-404__title">
+            <img src="<?=TPL?>/images/title-404.svg">
+        </div>
 
-require $_SERVER['DOCUMENT_ROOT'] . '/admin/footer.php';
+        <div class="page-404__text">Такой страницы нет, зато есть много дверей, которые можно найти у нас в каталоге.</div>
+
+        <a class="button _with-icon" href="/katalog/">
+            <span>В каталог</span>
+            <svg class="_icon-arrow-link">
+                <use xlink:href="/admin/templates/main/images/icons/arrow-link.svg#arrow-link"></use>
+            </svg>
+        </a>
+    </div>
+</section>
+
+<script>
+    $( 'h1' ).remove();
+    $( 'main > .container' ).css({
+        'padding': 0,
+        'max-width': 'none'
+    })
+</script>
+
+<?require $_SERVER['DOCUMENT_ROOT'] . '/admin/footer.php';
