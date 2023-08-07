126b6b0166a99839cd6b5946c0c25307ae5f7a49
Sh3nku <sh3nku@gmail.com>
2023-08-07 17:19:58 +0200

    fix schema breadcrumb

--- admin/templates/main/breadcrumb.php
+++ admin/templates/main/breadcrumb.php
@@ -9,7 +9,7 @@ if ( !empty( $arBreadcrumb ) ) {
     $htmlBreadcrumb .= '<nav class="breadcrumb"><ul class="breadcrumb__ul" itemscope itemtype="https://schema.org/BreadcrumbList">';
 
     foreach ( $arBreadcrumb as $key => $arItem ) {
-        $htmlBreadcrumb .= '<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">' . ( !empty( $arItem['url'] && count($arBreadcrumb ) != ( $key + 1 ) ) ? '<a itemprop="item" class="_underline" href="' . $arItem['url'] . '"><span itemprop="name"><meta itemprop="position" content="' . ( $key + 1 ) . '" />' . $arItem['title'] . '</span></a>' : '<span itemprop="name">' . $arItem['title'] . '</span>' ) . '</li>';
+        $htmlBreadcrumb .= '<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">' . ( !empty( $arItem['url'] && count($arBreadcrumb ) != ( $key + 1 ) ) ? '<a itemprop="item" class="_underline" href="' . $arItem['url'] . '"><span itemprop="name">' . $arItem['title'] . '</span></a>' : '<span itemprop="name">' . $arItem['title'] . '</span>' ) . '<meta itemprop="position" content="' . ( $key + 1 ) . '" /></li>';
     }
 
     $htmlBreadcrumb .= '</ul></nav>';
